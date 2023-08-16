8665091f01d94ba2f5b9aa59e815054063e6ad22
CelesteChua <133948504+CelesteChua@users.noreply.github.com>
2023-08-17 00:08:58 +0800

    Add files via upload

---
+++ learn_to_split - github/ls/datasets/__init__.py
@@ -0,0 +1,8 @@
+# from .NoisyMNIST import NoisyMNIST
+# from .Tox21 import Tox21
+# from .CelebA import CelebA
+# from .Waterbirds import Waterbirds
+# from .MNLI_bert_uncased import MNLI_bert_uncased
+# from .BeerReviews import BeerReviews
+# from .Vast import Vast
+from .Vast import Vast
